716f154938c38a958589f5783aa5f273c570508f
Arun Paudel <arun_paudel@sfu.ca>
2025-12-21 12:42:15 -0800

    implementing the stat class to oberve game and update status of win, loss, damage done, damge received and total fills

--- src/ca/SumAwesomeGame/model/stats/Stats.java
+++ src/ca/SumAwesomeGame/model/stats/Stats.java
@@ -12,9 +12,30 @@ import java.util.List;
 public class Stats implements GameObserver {
     private int gamesWon;
     private int gamesLost;
+    private int damageDone;
     private int damageReceived;
     private int totalNumberOfFills;
     private List<Weapon> weaponsUsed = new ArrayList<>();
+
+    public int getGamesWon() {
+        return gamesWon;
+    }
+
+    public int getGamesLost() {
+        return gamesLost;
+    }
+
+    public int getDamageDone() {
+        return damageDone;
+    }
+
+    public int getDamageReceived() {
+        return damageReceived;
+    }
+
+    public int getTotalNumberOfFills() {
+        return totalNumberOfFills;
+    }
 
     public void addUsedWeapon(Weapon currentWeapon) {
         weaponsUsed.add(currentWeapon);
@@ -23,7 +44,16 @@ public class Stats implements GameObserver {
     @Override
     public GameEvent update(GameEvent event) {
         GameEvent newEvent = new GameEvent(GameEvents.NO_NEW_EVENT);
-
+        switch (event.getEvent()){
+            case GAME_WON -> gamesWon++;
+            case GAME_LOSS -> gamesLost--;
+            case PLAYER_ATTACKED -> {
+                damageDone += event.getValue();
+                //TODO: add tracking for equipments
+            }
+            case ENEMY_ATTACKED -> damageReceived += event.getValue();
+            case FILL_COMPLETE -> totalNumberOfFills += event.getValue();
+        }
         return newEvent;
     }
 
